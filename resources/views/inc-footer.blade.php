1bfefc3697e83a4be03adec8756c95b3cfe762a6
laurentabbal <laurent@abbal.com>
2021-09-12 08:20:24 +0900

    footer mobile fix

--- resources/views/inc-footer.blade.php
+++ resources/views/inc-footer.blade.php
@@ -1,8 +1,8 @@
-<div class="container-fluid mt-2 mb-2 mt-5">
-	<div class="row">
-		<div class="col-md-12 text-center"><span style="cursor:pointer;" data-container="body" data-placement="top" data-toggle="popover" data-placement="top" data-html="true" data-content="<div class='text-justify'>Ce site n'utilise que deux 'cookies' nécessaires au fonctionnement du site.<br />Ils permettent de gérer la navigation et d'éviter les attaques extérieures par injection de code malicieux via un formulaire.<br />Ces 'cookies' ne contiennent aucune donnée personnelle et ils ont une durée de vie de trois heures. Ces 'cookies' sont conformes au RGPD et <a href='https://www.cnil.fr/fr/cookies-traceurs-que-dit-la-loi' target='_blank'>ne nécessitent pas de consentement de la part de l'utilisateur</a>.<br />De plus, ce site n'utilise par de système d'analyse de trafic.</div>">🍪<br /><span class="badge badge-warning small" style="padding-top:5px;">RGPD</span></span></div>
+<div class="container mb-2 mt-5">
+	<div class="row pt-5">
+		<div class="col-md-5 offset-md-2 text-center"><span style="cursor:pointer;" data-container="body" data-placement="top" data-toggle="popover" data-placement="top" data-html="true" data-content="<div class='text-justify'>Ce site n'utilise que deux 'cookies' nécessaires au fonctionnement du site.<br />Ils permettent de gérer la navigation et d'éviter les attaques extérieures par injection de code malicieux via un formulaire.<br />Ces 'cookies' ne contiennent aucune donnée personnelle et ils ont une durée de vie de trois heures. Ces 'cookies' sont conformes au RGPD et <a href='https://www.cnil.fr/fr/cookies-traceurs-que-dit-la-loi' target='_blank'>ne nécessitent pas de consentement de la part de l'utilisateur</a>.<br />De plus, ce site n'utilise par de système d'analyse de trafic.</div>">🍪<br /><span class="badge badge-warning small" style="padding-top:5px;">RGPD</span></span></div>
 	</div>
 	<div class="row pt-3">
-		<div class="col-md-12 text-center small"><a href="{{ route('donnees-personnelles') }}" class="text-muted">données personnelles</a> | <a href="mailto:contact@nuitducode.net" class="text-muted">contact</a></div>
+		<div class="col-md-5 offset-md-2 text-center small"><a href="{{ route('donnees-personnelles') }}" class="text-muted">données personnelles</a> | <a href="mailto:contact@nuitducode.net" class="text-muted">contact</a></div>
 	</div>
 </div>
