da7803ed0bc083411ed924ed16a02914ee7c1a1e
laurentabbal <laurent@abbal.com>
2022-02-28 09:16:09 +0900

    console maj

--- resources/views/console.blade.php
+++ resources/views/console.blade.php
@@ -30,6 +30,12 @@
 
                     @csrf
 
+                    <?php
+                    $datetime = new DateTime(Auth::user()->ndc_date);
+                    $jour = $datetime->format('j');
+                    $mois = $datetime->format('m');
+                    $heure = $datetime->format('H').':'.$datetime->format('i');
+                    ?>
                     <table class="table table-borderless table-sm table-responsive">
                         <tr>
                             <td style="width:160px;"></td>
@@ -40,37 +46,28 @@
                             <td class="text-right">Date</td>
                             <td class="text-center">
                                 <select id="ndc_jour" name="ndc_jour" class="form-control form-control-sm">
-                                    @for ($j = 1; $j <= 31; $j++) {
-                                    <option value="{{$j}}">{{ $j }}</option>
+                                    @for ($j = 1; $j <= 31; $j++)
+                                    <option value="{{$j}}" @if($jour == $j) selected @endif>{{ $j }}</option>
                                     @endfor
-                                </select></td>
+                                </select>
+                            </td>
                             <td class="text-center">
                                 <select id="ndc_mois" name="ndc_mois" class="form-control form-control-sm">
-                                    <option value="avril">avril</option>
-                                    <option value="mai">mai</option>
-                                    <option value="juin">juin</option>
+                                    <option value="04" @if($mois == '04') selected @endif>avril</option>
+                                    <option value="05" @if($mois == '05') selected @endif>mai</option>
+                                    <option value="06" @if($mois == '06') selected @endif>juin</option>
                                 </select>
                             </td>
                         </tr>
                         <tr>
-                            <td class="text-right pt-4" style="width:160px;">Heure de fin <sup><i class="fas fa-question-circle text-muted" style="cursor:help" data-toggle="tooltip" data-placement="right" title="Cette indication sera utilisée pour vérifier que des jeux n'auront pas été modifiés après le temps limite."></i></sup></td>
-                            <td class="text-center pt-4">
+                            <td class="text-right pt-3" style="width:160px;">Heure de fin <sup><i class="fas fa-question-circle text-muted" data-toggle="tooltip" data-placement="right" title="Cette indication sera utilisée pour vérifier que des jeux n'auront pas été modifiés après le temps limite."></i></sup></td>
+                            <td class="text-center pt-3">
                                 <select id="ndc_heure_fin" name="ndc_heure_fin" class="form-control form-control-sm">
-                                    @for ($h = 12; $h <= 24; $h++) {
-                                    <option value="{{$h}}00">{{$h}}h00</option>
-                                    <option value="{{$h}}h30">{{$h}}h30</option>
+                                    @for ($h = 12; $h <= 23; $h++)
+                                    <option value="{{$h}}:00" @if($heure == $h.':00') selected @endif>{{$h}}h00</option>
+                                    <option value="{{$h}}:30" @if($heure == $h.':30') selected @endif>{{$h}}h30</option>
                                     @endfor
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="text-right">Heure de fin Cycle 3 <sup><i class="fas fa-question-circle text-muted" style="cursor:help" data-toggle="tooltip" data-placement="right" title="Si, pour les élèves de Cycle 3, un temps plus court leur a été proposé (4h ou lieu de 6 par exemple), l'heure de fin peut être différente de celle des autres catégories d'élèves."></i></sup></td>
-                            <td class="text-center">
-                                <select id="ndc_heure_fin_c3" name="ndc_heure_fin_c3" class="form-control form-control-sm">
-                                    @for ($h = 12; $h <= 24; $h++) {
-                                    <option value="{{$h}}00">{{$h}}h00</option>
-                                    <option value="{{$h}}h30">{{$h}}h30</option>
-                                    @endfor
+                                    <option value="00:00" @if($heure == '00:00') selected @endif>minuit</option>
                                 </select>
                             </td>
                         </tr>
@@ -79,7 +76,6 @@
                             <td></td>
                         </tr>
                     </table>
-
 
 
                 <h2>SCRATCH</h2>
@@ -91,12 +87,12 @@
                             <td class="small text-center text-muted">Nombre<br />d'élèves</td>
                         </tr>
                         <tr>
-                            <td class="text-right">Cycle 3 (CM1 > 6<sup>e</sup>)</td>
+                            <td class="text-right">Cycle 3 <small>(CM1 > 6<sup>e</sup>)</small></td>
                             <td class="text-center"><input id="scratch_nb_equipes_c3" name="scratch_nb_equipes_c3" class="form-control form-control-sm" style="display:inline;width:40px" value="{{Auth::user()->scratch_nb_equipes_c3}}" /></td>
                             <td class="text-center"><input id="scratch_nb_eleves_c3" name="scratch_nb_eleves_c3" class="form-control form-control-sm" style="display:inline;width:40px" value="{{Auth::user()->scratch_nb_eleves_c3}}" /></td>
                         </tr>
                         <tr>
-                            <td class="text-right">Cycle 4 (5<sup>e</sup> > 3<sup>e</sup>)</td>
+                            <td class="text-right">Cycle 4 <small>(5<sup>e</sup> > 3<sup>e</sup>)</small></td>
                             <td class="text-center"><input id="scratch_nb_equipes_c4" name="scratch_nb_equipes_c4" class="form-control form-control-sm" style="display:inline;width:40px" value="{{Auth::user()->scratch_nb_equipes_c4}}" /></td>
                             <td class="text-center"><input id="scratch_nb_eleves_c4" name="scratch_nb_eleves_c4" class="form-control form-control-sm" style="display:inline;width:40px" value="{{Auth::user()->scratch_nb_eleves_c4}}" /></td>
                         </tr>
@@ -113,10 +109,7 @@
                     </table>
 
 
-
                 <h2>PYTHON</h2>
-
-
 
                     <table class="table table-borderless table-sm table-responsive">
                         <tr>
@@ -125,12 +118,12 @@
                             <td class="small text-center text-muted">Nombre<br />d'élèves</td>
                         </tr>
                         <tr>
-                            <td class="text-right">Première NSI <sup><i class="fas fa-question-circle text-muted" style="cursor:help" data-toggle="tooltip" data-placement="right" title="Élèves de Première NSI ou élèves ayant les connaissances suffisantes en programmation impérative Python"></i></sup></td>
+                            <td class="text-right">Première NSI <sup><i class="fas fa-question-circle text-muted" data-toggle="tooltip" data-placement="right" title="Élèves de Première NSI ou élèves ayant les connaissances suffisantes en programmation impérative Python"></i></sup></td>
                             <td class="text-center"><input  id="python_nb_equipes_pi" name="python_nb_equipes_pi" class="form-control form-control-sm" style="display:inline;width:40px" /></td>
                             <td class="text-center"><input  id="python_nb_eleves_pi" name="python_nb_eleves_pi" class="form-control form-control-sm" style="display:inline;width:40px" /></td>
                         </tr>
                         <tr>
-                            <td class="text-right">Terminale NSI <sup><i class="fas fa-question-circle text-muted" style="cursor:help" data-toggle="tooltip" data-placement="right" title="Élèves de Terminale NSI ou élèves ayant les connaissances suffisantes en programmation orientée objet Python"></i></sup></td>
+                            <td class="text-right">Terminale NSI <sup><i class="fas fa-question-circle text-muted" data-toggle="tooltip" data-placement="right" title="Élèves de Terminale NSI ou élèves ayant les connaissances suffisantes en programmation orientée objet Python"></i></sup></td>
                             <td class="text-center"><input  id="python_nb_equipes_poo" name="python_nb_equipes_poo" class="form-control form-control-sm" style="display:inline;width:40px" /></td>
                             <td class="text-center"><input  id="python_nb_eleves_poo" name="python_nb_eleves_poo" class="form-control form-control-sm" style="display:inline;width:40px" /></td>
                         </tr>
@@ -142,7 +135,6 @@
                     </table>
 
                 </form>
-
 
             </div>
 
