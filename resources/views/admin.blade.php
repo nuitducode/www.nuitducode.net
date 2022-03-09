663a4c8c42b3862b7914a637dbd60cb4c2fed5ad
laurentabbal <laurent@abbal.com>
2022-03-09 18:54:05 +0900

    Update admin.blade.php

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -13,13 +13,13 @@ if (Auth::user()->is_admin != 1) {
 </head>
 <body>
 
-    @include('inc-nav-console') 
+    @include('inc-nav-console')
 
 	<div class="container mt-3 mb-5">
 		<div class="row">
 
             <div class="col-md-2 mt-4">
-                <a class="btn btn-light btn-sm mb-4" href="{{ url()->previous() }}" role="button"><i class="fas fa-arrow-left"></i></a>
+                <a class="btn btn-light btn-sm mb-4" href="/console" role="button"><i class="fas fa-arrow-left"></i></a>
                 <a class=" btn btn-light btn-sm btn-block text-left" href="/console/fiche-inscription" role="button"><i class="far fa-address-card pr-2"></i> fiche d'inscription</a>
                 <a class=" btn btn-light btn-sm btn-block text-left" href="https://github.com/nuitducode/ORGANISATION/discussions" target="_blank" role="button"><i class="far fa-comment-alt pr-2"></i> discussions</a>
                 <div class="text-monospace text-muted small mt-4">JEUX</div>
