c2807318b131cc109f3dc33d92fc37c39e72a449
laurentabbal <laurent@abbal.com>
2022-04-04 15:04:47 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,21 @@
 <head>
 	@include('inc-meta')
     <title>{{ config('app.name', 'Laravel') }}</title>
+	<!-- Matomo - données anonymisées - pas de cookies - RGPD -->
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="//www.awame.net/matomo/";
+			_paq.push(['setTrackerUrl', u+'matomo.php']);
+			_paq.push(['setSiteId', '10']);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<!-- End Matomo Code -->
 </head>
 <body>
 
