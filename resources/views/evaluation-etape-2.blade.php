3bf85f4a57e628aaa4cc87793a57e345b64a12e9
laurentabbal <laurent@abbal.com>
2022-05-06 18:32:55 +0900

    ~

--- resources/views/evaluation-etape-2.blade.php
+++ resources/views/evaluation-etape-2.blade.php
@@ -66,12 +66,12 @@
                                             <h3 class="mb-1 mt-1">[NdC 2022 - C3] {{$jeu_scratch->title}}</h3>
                                             <div class="text-monospace small">Création : {{$jeu_scratch->history->created}}</div>
                                             <div class="text-monospace small">Derniere modification : {{$jeu_scratch->history->modified}}</div>
-                                            <div class="text-monospace small">Si le jeu ne s'affiche pas correctement, vous pouvez l'ouvrir dans un autre onglet en cliquant <a href="https://scratch.mit.edu/projects/{{$jeu_scratch->id}}" target="_blank">ici</a>.</div>
                                         </div>
                                     </div>
                                     @endif
                                     <div class="row">
                                         <div class="col-md-6">
+                                            <div class="text-monospace small">Si le jeu ne s'affiche pas correctement, vous pouvez l'ouvrir dans un autre onglet en cliquant <a href="https://scratch.mit.edu/projects/{{$jeu_scratch->id}}" target="_blank">ici</a>.</div>
                                             <iframe src="https://scratch.mit.edu/projects/{{$jeu->scratch_id}}/embed" allowtransparency="true" width="100%" height="402" frameborder="0" scrolling="no" allowfullscreen></iframe>
                                             <div class="small text-monospace" style="border:1px solid silver; padding:10px;border-radius:4px; background-color:white;">{{$jeu_scratch->instructions}}</div>
                                             @if($jury_type != 'eleve')
