ef9302a340c99ba5339d90434dfe85605ec405c9
laurentabbal <laurent@abbal.com>
2022-05-06 15:39:22 +0900

    ~

--- resources/views/evaluation-etape-2.blade.php
+++ resources/views/evaluation-etape-2.blade.php
@@ -183,16 +183,15 @@
 
                                     <div class="row">
                                         <div class="col-md-6">
-<pre class="m-0"><code id="htmlViewer" style="color:rgb(216, 222, 233); font-weight:400;background-color:rgb(46, 52, 64);background:rgb(46, 52, 64);display:block;padding: 1.5em;border-radius:5px;"><span style="color:rgb(129, 161, 193); font-weight:400;">import</span> requests
-<span style="color:rgb(129, 161, 193); font-weight:400;">import</span> os
+<pre class="m-0"><code id="htmlViewer" style="color:rgb(216, 222, 233); font-weight:400;background-color:rgb(46, 52, 64);background:rgb(46, 52, 64);display:block;padding: 1.5em;border-radius:5px;"><span style="color:rgb(129, 161, 193); font-weight:400;">import</span> requests, os
 # ================
 code = <span style="color:rgb(163, 190, 140); font-weight:400;">'{{$jeu->etablissement_jeton}}-{{$jeu->python_id}}'</span>
 # ================
 site = <span style="color:rgb(163, 190, 140); font-weight:400;">'https://www.nuitducode.net'</span>
 chemin = <span style="color:rgb(163, 190, 140); font-weight:400;">'/storage/fichiers_pyxel/'</span>
 @foreach($files as $file)
-<span style="color:rgb(129, 161, 193); font-weight:400;">{{pathinfo($file, PATHINFO_EXTENSION)}}</span> = requests.<span style="color:rgb(129, 161, 193); font-weight:400;">get</span>(site + chemin + {{basename($file)}})
-with <span style="color:rgb(129, 161, 193); font-weight:400;">open</span>({{basename($file)}}, <span style="color:rgb(163, 190, 140); font-weight:400;">'wb'</span>) <span style="color:rgb(129, 161, 193); font-weight:400;">as</span> file:
+<span style="color:rgb(129, 161, 193); font-weight:400;">{{pathinfo($file, PATHINFO_EXTENSION)}}</span> = requests.<span style="color:rgb(129, 161, 193); font-weight:400;">get</span>(site + chemin + '{{basename($file)}}')
+with <span style="color:rgb(129, 161, 193); font-weight:400;">open</span>('{{basename($file)}}', <span style="color:rgb(163, 190, 140); font-weight:400;">'wb'</span>) <span style="color:rgb(129, 161, 193); font-weight:400;">as</span> file:
     file.write(<span style="color:rgb(129, 161, 193); font-weight:400;">{{pathinfo($file, PATHINFO_EXTENSION)}}</span>.content)
 @endforeach
 @foreach($files as $file)
