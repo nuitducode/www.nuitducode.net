0325f3a34e8f00e5057e95ef25730db409243456
laurentabbal <laurent@abbal.com>
2022-05-07 14:58:47 +0900

    ~

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -90,9 +90,9 @@ if (Auth::user()->is_admin != 1) {
                                 <td>{{$etablissement->id}}</td>
                                 <td>{{$etablissement->jeton}}</td>
                                 <td><a href="/ndc/{{$etablissement->jeton[3].'j'.$etablissement->jeton[2].'w'.$etablissement->jeton[1].'a'.$etablissement->jeton[0]}}">{{$etablissement->jeton[3].'j'.$etablissement->jeton[2].'w'.$etablissement->jeton[1].'a'.$etablissement->jeton[0]}}</a></td>
-                                <td class="text-center"><a href="/consloe/admin_jeux?id={{$etablissement->id}}"><i class="fas fa-gamepad"></i></a></td>
-                                <td class="text-center"><a href="/consloe/admin_evaluations?id={{$etablissement->id}}"><i class="fas fa-check"></i></a></td>
-                                <td class="text-center"><a href="/consloe/admin_notes?id={{$etablissement->id}}"><i class="fas fa-marker"></i></a></td>
+                                <td class="text-center"><a href="/console/admin_jeux?id={{$etablissement->id}}"><i class="fas fa-gamepad"></i></a></td>
+                                <td class="text-center"><a href="/console/admin_evaluations?id={{$etablissement->id}}"><i class="fas fa-check"></i></a></td>
+                                <td class="text-center"><a href="/console/admin_notes?id={{$etablissement->id}}"><i class="fas fa-marker"></i></a></td>
                                 <td>{{$etablissement->prenom}}</td>
                                 <td>{{$etablissement->nom}}</td>
                                 <td>{{$etablissement->pays}}</td>
