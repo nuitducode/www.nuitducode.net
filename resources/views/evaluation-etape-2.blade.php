ab86999b678fcc08710dd3fb96dfe6ab441aa1bb
laurentabbal <laurent@abbal.com>
2022-05-07 15:10:23 +0900

    ~

--- resources/views/evaluation-etape-2.blade.php
+++ resources/views/evaluation-etape-2.blade.php
@@ -74,7 +74,13 @@
                                         <div class="col-md-6 text-center">
                                             <div><img src="https://uploads.scratch.mit.edu/get_image/project/{{$jeu->scratch_id}}_282x218.png" class="img-fluid" style="border-radius:4px;" width="100%" /></div>
                                             <button type="button" class="btn btn-success btn-sm mt-2 mb-3" onClick="this.previousElementSibling.innerHTML='<iframe src=\'https://scratch.mit.edu/projects/{{$jeu->scratch_id}}/embed\' width=\'100%\' height=\'402\' frameborder=\'0\' scrolling=\'no\'></iframe>'">afficher / recharger le jeu</button>
-                                            <div class="small text-monospace" style="border:1px solid silver; padding:10px;border-radius:4px; background-color:white;">{{$jeu_scratch->instructions}}</div>
+                                            <div class="small text-monospace text-left" style="border:1px solid silver; padding:10px;border-radius:4px; background-color:white;">
+                                                @if ($jeu_scratch->instructions != NULL)
+                                                    {{$jeu_scratch->instructions}}
+                                                @else
+                                                    <span class="text-danger small text-monospace">pas d'instructions</span>
+                                                @endif
+                                            </div>
                                             @if($jury_type != 'eleve')
                                             <div class="text-monospace small text-muted pt-1 pl-1">
                                                 <i class="fas fa-gamepad" style="font-size:140%;vertical-align:-1px;"></i> <a href="https://scratch.mit.edu/projects/{{$jeu_scratch->id}}" target="_blank">{{$jeu_scratch->id}}</a> ~
