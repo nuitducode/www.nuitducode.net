b5058a5a8284436ae998b696f2d785969a274e1f
laurentabbal <laurent@abbal.com>
2022-05-08 14:16:04 +0900

    ~

--- resources/views/jouer-jeu-pyxel.blade.php
+++ resources/views/jouer-jeu-pyxel.blade.php
@@ -9,20 +9,14 @@
 
         @include('inc-nav-console')
 
-    	<div class="container mt-3 mb-5">
+        <div class="container mt-4 mb-5">
     		<div class="row">
 
                 <div class="col-md-2 mt-4">
                     <a class="btn btn-light btn-sm mb-4" href="{{ url()->previous() }}" role="button"><i class="fas fa-arrow-left"></i></a>
-                    <a class=" btn btn-light btn-sm btn-block text-left" href="/console/fiche-inscription" role="button"><i class="far fa-address-card pr-2"></i> fiche d'inscription</a>
-                    <a class=" btn btn-light btn-sm btn-block text-left" href="https://github.com/nuitducode/ORGANISATION/discussions" target="_blank" role="button"><i class="far fa-comment-alt pr-2"></i> discussions</a>
-                    <div class="text-monospace text-muted small mt-4">JEUX</div>
-                    <a class=" btn btn-light btn-sm btn-block text-left" href="/console/ndc" role="button">Nuit du c0de</a>
-                    <a class=" btn btn-light btn-sm btn-block text-left mt-1" href="/console/sltn" role="button">Sélections</a>
-                    <a class=" btn btn-light btn-sm btn-block text-left mt-3" href="/console/bas" role="button">Bac à sable</a>
                 </div>
 
-    			<div class="col-md-9 offset-md-1 mt-4">
+    			<div class="col-md-10">
 
 <?php
 if(File::exists(storage_path("app/public/fichiers_pyxel/".$jeu_id))) {
