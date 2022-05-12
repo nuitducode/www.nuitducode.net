477d862631c432ddd0c936e70d99f9c7b724823f
laurentabbal <laurent@abbal.com>
2022-05-12 21:36:02 +0900

    ~

--- resources/views/admin_notes.blade.php
+++ resources/views/admin_notes.blade.php
@@ -91,8 +91,8 @@ if (Auth::user()->is_admin != 1) {
 
                                     @else
                                         <div class="text-monospace small text-danger">Cet identifiant Scratch n'existe pas! [{{$jeu->scratch_id}}]</div>
+                                        <div class="text-monospace small text-danger">Vérifier que le jeu a bien été partagé (bouton orange "Partager", ou "Share" en anglais).</div>
                                     @endif
-
                                 </div>
                             </div>
                             @endforeach
