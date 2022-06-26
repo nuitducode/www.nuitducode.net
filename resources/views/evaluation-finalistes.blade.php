52eb482e0d93aa9a1ad0398c16b3d77d3b8fda23
laurentabbal <laurent@abbal.com>
2022-06-26 12:22:41 +0900

    ~

--- resources/views/evaluation-finalistes.blade.php
+++ resources/views/evaluation-finalistes.blade.php
@@ -40,7 +40,7 @@
                 $categorie = Crypt::decryptString($categorie);
 
                 // JEUX A EXCLURE
-                $excluded_games = [660, 762, 569, 676, 470, 520, 540, 724, 411, 779, 313, 207, 225, 156, 730, 260, 364, 562, 578, 603, 324, 715, 667, 778];
+                $excluded_games = [762, 569, 676, 470, 520, 540, 724, 411, 779, 313, 207, 225, 156, 730, 364, 562, 578, 603, 324, 715, 667, 778];
                 // Exclure les jeux deja evalues par l'utilisateur
                 $excluded_games = array_merge($excluded_games, App\Models\Evaluation_finaliste::where([['jury_id', Auth::user()->id], ['categorie', $categorie]])->pluck('game_id')->toArray());
                 // Exclure les jeux evalues 5 fois
