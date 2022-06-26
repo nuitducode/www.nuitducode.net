fd5d31e2bd14e86a5081c735454f48d7413cd67d
laurentabbal <laurent@abbal.com>
2022-06-26 10:03:59 +0900

    ~

--- resources/views/admin_finalistes.blade.php
+++ resources/views/admin_finalistes.blade.php
@@ -54,7 +54,7 @@ if (Auth::user()->is_admin != 1) {
 
                                 @foreach($evaluations AS $id => $evaluation)
                                     <div class="col mb-4">
-                                        <div class="card p-3" @if(($loop->iteration == 1 OR $loop->iteration == 2) AND $evaluation['note'] != 0) style="background-color:#ffc905;border-radius:5px;" @endif>
+                                        <div class="card p-3" @if(($loop->iteration <= 8) AND $evaluation['note'] != 0) style="background-color:#ffc905;border-radius:5px;" @endif>
 
                                             <h3 class="mt-0" style="@if(in_array($id, $excluded_games)) text-decoration: line-through; @endif color:#4cbf56">@if(($loop->iteration == 1 OR $loop->iteration == 2) AND $evaluation['note'] != 0)<i class="fas fa-crown mr-1" style="color:#f39c12"></i>@endif {{$evaluation['nom_equipe']}}</h3>
                                             <p class="text-monospace text-muted small">[{{$id}}]</p>
@@ -118,7 +118,7 @@ if (Auth::user()->is_admin != 1) {
                                 @foreach($evaluations AS $id => $evaluation)
 
                                     <div class="col mb-4">
-                                        <div class="card p-3" @if(($loop->iteration == 1 OR $loop->iteration == 2) AND $evaluation['note'] != 0) style="background-color:#ffc905;border-radius:5px;" @endif>
+                                        <div class="card p-3" @if(($loop->iteration <= 8) AND $evaluation['note'] != 0) style="background-color:#ffc905;border-radius:5px;" @endif>
 
                                             <h3 class="mt-0" style="@if(in_array($id, $excluded_games)) text-decoration: line-through; @endif color:#4cbf56">@if(($loop->iteration == 1 OR $loop->iteration == 2) AND $evaluation['note'] != 0)<i class="fas fa-crown mr-1" style="color:#f39c12"></i>@endif {{ $evaluation['nom_equipe'] }}</h3>
                                             <p class="text-monospace text-muted small">[{{$id}}]</p>
